cf0ea7a9c9bd1c60f131a82f4870910d7874248e
Tsvetomir Shterev <62944181+TsvetomirShterev@users.noreply.github.com>
2022-05-18 17:52:20 +0300

    Add data access for remaining microservices

---
+++ CloudWeather.Report/DataAccess/WeatherReportDbContext.cs
@@ -0,0 +1,32 @@
+﻿namespace CloudWeather.Report.DataAccess;
+
+using Microsoft.EntityFrameworkCore;
+
+public class WeatherReportDbContext : DbContext
+{
+    public WeatherReportDbContext()
+    {
+
+    }
+
+    public WeatherReportDbContext(DbContextOptions opts) : base(opts)
+    {
+
+    }
+
+    public DbSet<WeatherReport> WeatherReports { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WeatherReport>(w =>
+        {
+            w.ToTable("weather_report");
+        });
+    }
+}
